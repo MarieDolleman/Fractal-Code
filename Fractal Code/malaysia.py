8bda8732390679c76d27572937bf38e7b1fcfa7b
Marie Dolleman <marie.dolleman@drake.edu>
2017-10-18 22:39:31 -0500

    I HAVE A FRACTAL ITS A WIP

--- Fractal Code/malaysia.py
+++ Fractal Code/malaysia.py
@@ -1,21 +1,57 @@
 
 import turtle
 
-def malaysia(step, rule, angle, depth, t):
+def original_shape(step, rule, angle, depth, t):
+    t.forward(step)
+    t.left(120)
+    t.forward(step)
+    t.left(120)
+    t.forward(step)
+    #fractal(step, rule, angle, depth, t)
+
+def fractal(step, rule, angle, depth, t):
     if depth > 0:
-        a = lambda: malaysia(step, "a", angle, depth - 1, t)
-        b = lambda: malaysia(step, "b", angle, depth - 1, t)
+        a = lambda: fractal(step, "a", angle, depth - 1, t)
+        b = lambda: fractal(step, "b", angle, depth - 1, t)
         left = lambda: t.left(angle)
         right = lambda: t.right(608)
         forward = lambda: t.forward(step)
         #back = lambda: t.back(step)
         if rule == "a":
-            for i in range(16):
-                left(); forward(); b(); left; forward(); #back
+            forward();
         if rule == "b":
-            for i in range(6):
-                left(); forward(); a(); left; forward();
+            a(); 
 
+# three terms in model, so we start with triangle
 myTurtle = turtle.Turtle()
+myTurtle.hideturtle()
+myTurtle.setx(-360)
+ts = turtle.Screen()
+print(myTurtle.position())
+print(ts.window_width())
+print(ts.window_height())
 myTurtle.speed(0)
-malaysia(10, "a", 256, 5, myTurtle)
+#original_shape(100, "a", 256, 5, myTurtle)
+
+
+# a = -
+# b = angle leftwards
+
+instructions = ['a']
+for depth in range(5):
+    for index, letter in enumerate(instructions):
+        if letter == 'a':
+            instructions[index] = list('abacaba')
+    flattened_instructions = [item for sublist in instructions for item in sublist]
+    instructions = flattened_instructions
+
+for letter in flattened_instructions:
+    if letter == 'a':
+        myTurtle.forward(10)
+    if letter == 'b':
+        myTurtle.left(60)
+    if letter == 'c':
+        myTurtle.left(-120)
+
+
+ts.exitonclick()
